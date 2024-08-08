d6c5320dddece98460a1b24a7670523c6b884f05
Eduardo Firvida <efirvida@gmail.com>
2024-08-08 17:01:12 -0300

    Add CHT tutorial flow-over-heated-plate
    At least its running and changing data between participants
    but needs a revision of the FEniCSx participant implementation

--- tutorial/flow-over-heated-plate/solid-fenics/solidx.py
+++ tutorial/flow-over-heated-plate/solid-fenics/solidx.py
@@ -1,14 +1,14 @@
-import matplotlib as mpl
 import ufl
 import numpy as np
 import os
+import shutil
 
 from petsc4py import PETSc
 from mpi4py import MPI
 
 from dolfinx import fem, io
-from dolfinx.mesh import CellType, create_box,locate_entities_boundary
-from dolfinx.fem.petsc import assemble_vector, assemble_matrix, create_vector, apply_lifting, set_bc
+from dolfinx.mesh import CellType, create_box, locate_entities_boundary
+from dolfinx.fem.petsc import LinearProblem
 from fenicsxprecice import Adapter, DiscreteLinearProblem
 
 
@@ -16,25 +16,29 @@ MPI_COMM = MPI.COMM_WORLD
 CURRENT_FOLDER = os.path.dirname(__file__)
 PARTICIPANT_CONFIG = os.path.join(CURRENT_FOLDER, "precice-adapter-config.json")
 RESULTS_DIR = os.path.join(CURRENT_FOLDER, "results")
+
+if os.path.isdir(RESULTS_DIR):
+    shutil.rmtree(RESULTS_DIR)
 os.makedirs(RESULTS_DIR, exist_ok=True)
 os.chdir(CURRENT_FOLDER)
 
 WRITER = io.VTKFile(MPI_COMM, f"{RESULTS_DIR}/result.pvd", "w")
 
+alpha = 1  # m^2/s, https://en.wikipedia.org/wiki/Thermal_diffusivity
+k = 100  # kg * m / s^3 / K, https://en.wikipedia.org/wiki/Thermal_conductivity
+T_hot = 310
+dt_out = 0.2  # same as fluid
 
-# Define temporal parameters
-t = 0  # Start time
-T = 1.0  # Final time
-num_steps = 50
-dt = T / num_steps  # time step size
+# ------- #
+# MESHING #
+# ------- #
 
-# Define mesh
 nx = 100
 ny = 25
 nz = 1
 
 y_top = 0
-y_bottom = y_top - .25
+y_bottom = y_top - 0.25
 x_left = 0
 x_right = x_left + 1
 
@@ -46,15 +50,8 @@ domain = create_box(
 )
 dim = domain.topology.dim
 V = fem.functionspace(domain, ("Lagrange", 2))
-
-u_n = fem.Function(V)
-u_n.name = "u_n"
-
-
-# ------------------- #
-# Boundary conditions #
-# ------------------- #
-tol = 1e-14
+V_g = fem.functionspace(domain, ("Lagrange", 2, (3,)))
+WRITER.write_mesh(domain)
 
 
 def bottom_boundary_fn(x):
@@ -65,77 +62,70 @@ def neumann_boundary(x):
     return np.isclose(x[1], y_top)
 
 
+def determine_heat_flux(V_g, u_g):
+    w = ufl.TrialFunction(V_g)
+    v = ufl.TestFunction(V_g)
+
+    a = ufl.inner(w, v) * ufl.dx
+    L = ufl.inner(-k * ufl.grad(u_g), v) * ufl.dx
+    problem = LinearProblem(a, L)
+    return problem.solve()
+
 
 bottom_boundary = fem.locate_dofs_geometrical(V, bottom_boundary_fn)
 coupling_boundary = locate_entities_boundary(domain, dim - 1, neumann_boundary)
 
-bcs = [fem.dirichletbc(PETSc.ScalarType(0), bottom_boundary, V)]
+bcs = [fem.dirichletbc(PETSc.ScalarType(T_hot), bottom_boundary, V)]
 
 # ------------ #
 # PRECICE INIT #
 # ------------ #
-participant = Adapter(MPI_COMM, PARTICIPANT_CONFIG, domain)
+participant = Adapter(PARTICIPANT_CONFIG, domain)
 participant.initialize(V, coupling_boundary)
-np.savetxt("dofs.txt", participant.interface_coordinates, delimiter=",", header="x,y,z")
 dt = participant.dt
-
-
-
-
-
-
-# Create boundary condition
-fdim = domain.topology.dim - 1
-boundary_facets = locate_entities_boundary(
-    domain, fdim, lambda x: np.full(x.shape[1], True, dtype=bool))
-bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V)
-
-WRITER.write_mesh(domain)
 
 # Define solution variable, and interpolate initial solution for visualization in Paraview
 uh = fem.Function(V)
-uh.name = "uh"
-uh.interpolate(initial_condition)
-WRITER.write_function(uh, t)
+u_n = fem.Function(V, name="T")
 
-u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
-f = fem.Constant(domain, PETSc.ScalarType(0))
-a = u * v * ufl.dx + dt * ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
-L = (u_n + dt * f) * v * ufl.dx
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+F = u * v / dt * ufl.dx + alpha * ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx - u_n * v / dt * ufl.dx
+a, L = ufl.lhs(F), ufl.rhs(F)
 
-bilinear_form = fem.form(a)
-linear_form = fem.form(L)
+heat_problem = DiscreteLinearProblem(a=a, L=L, u=uh, bcs=bcs, point_dofs=participant.interface_dof)
 
-A = assemble_matrix(bilinear_form, bcs=[bc])
-A.assemble()
-b = create_vector(linear_form)
+t = 0.0
+while participant.is_coupling_ongoing():
+    if participant.requires_writing_checkpoint():  # write checkpoint
+        participant.store_checkpoint((u_n, t))
 
-solver = PETSc.KSP().create(domain.comm)
-solver.setOperators(A)
-solver.setType(PETSc.KSP.Type.PREONLY)
-solver.getPC().setType(PETSc.PC.Type.LU)
-
-for i in range(num_steps):
-    t += dt
-
-    # Update the right hand side reusing the initial vector
-    with b.localForm() as loc_b:
-        loc_b.set(0)
-    assemble_vector(b, linear_form)
-
-    # Apply Dirichlet boundary condition to the vector
-    apply_lifting(b, [bilinear_form], [[bc]])
-    b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
-    set_bc(b, [bc])
-
-    # Solve linear problem
-    solver.solve(b, uh.vector)
+    read_data = participant.read_data(dt)
+    heat_problem.solve()
+    u_flux = determine_heat_flux(V_g, uh)
+    u_flux.name = "Heat Flux"
     uh.x.scatter_forward()
+    u_flux.x.scatter_forward()
 
-    # Update solution at previous time step (u_n)
-    u_n.x.array[:] = uh.x.array
+    # Only exchange y component of flux
+    participant.write_data(u_flux.sub(1))
 
-    # Write solution to file
-    WRITER.write_function(uh, t)
-    # Update plot
+    # Call to advance coupling, also returns the optimum time step value
+    participant.advance(dt)
+
+    if participant.requires_reading_checkpoint():
+        u_cp, t_cp = participant.retrieve_checkpoint()
+        u_n.x.array[:] = u_cp.x.array
+        t = t_cp
+    else:
+        u_n.x.array[:] = uh.x.array
+        t += dt
+
+    if participant.is_time_window_complete():
+        tol = 10e-5  # we need some tolerance, since otherwise output might be skipped.
+        if abs((t + tol) % dt_out) < 2 * tol:
+            WRITER.write_function([u_n, u_flux], t)
+            WRITER.close()
+
 WRITER.close()
+participant.finalize()
